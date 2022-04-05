99d3ec4430c792e041f0ff3282905541b3792a6c
moin malik <moin.malik@tothenew.com>
2022-04-05 21:15:22 +0530

    sidebar added

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,87 @@
-import React from 'react';
-import "./sidebar.css"
-import {RssFeed, Chat, PlayCircleOutlined,} from "@material-ui/icons"
+import React from "react";
+import "./sidebar.css";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
-        <h2>hello wrold </h2>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Chat</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledOutlined className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarbarListItemText">Courses</span>
+          </li>
+        </ul>
+
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/1.jpg" alt="" />
+            <span className="sidebarFriendName">marry</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/1.jpg" alt="" />
+            <span className="sidebarFriendName">marry</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/1.jpg" alt="" />
+            <span className="sidebarFriendName">marry</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/1.jpg" alt="" />
+            <span className="sidebarFriendName">marry</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/1.jpg" alt="" />
+            <span className="sidebarFriendName">marry</span>
+          </li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
