1b85d868a967b489e6bcd5413d34d1ae5d42056c
mayank <mayank.rawat1@tothenew.com>
2022-04-06 11:28:04 +0530

    login backend partial

---
+++ api/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const authusers = require("./routes/authUser");
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+//middleware
+app.use(express.json());
+app.use("/authusers", authusers);
+
+//database connection
+require("./db/connection");
+
+//userAuthModel
+const UserAuthModel = require("./models/userAuthModel");
+
+app.listen(port, () => {
+  console.log("server listen...");
+});
