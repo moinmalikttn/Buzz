c2f0c21b0aff0a2b92b4b9b6a87e5d2a6d1f4764
moin malik <moin.malik@tothenew.com>
2022-04-07 10:35:59 +0530

    home added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import Profile from "./pages/profile/Profile";
-import { Router, Routes, Route } from "react-router-dom"
-
-
+import Login from "./pages/login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        {/* <Route exact path="/" element={<Login />} /> */}
-        <Route path="/home" element={<Home />} />
-        {/* <Route exact path="/Profile/:username" element={<Profile />} /> */}
-
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/feeds" element={<Home />} />
+        
       </Routes>
-    </Router>
+    </div>
+    </Router >
   );
 }
